bab6a9df8ce48eeef2eb782a6edb2c0240cb31d4
NehuenV <32799856+NehuenV@users.noreply.github.com>
2018-09-24 14:34:15 -0300

    funcion y usuario abm

--- TicketUNLa/src/negocio/FuncionABM.java
+++ TicketUNLa/src/negocio/FuncionABM.java
@@ -1,5 +1,40 @@
 package negocio;
 
-public class FuncionABM {
+import java.util.GregorianCalendar;
+import java.util.List;
 
+import dao.FuncionDao;
+import datos.CodigoDescuento;
+import datos.DiaDescuento;
+import datos.Evento;
+import datos.Funcion;
+
+public class FuncionABM {
+	
+	FuncionDao dao = new FuncionDao(); 
+	
+	public Funcion traerEntrada(int idFuncion)throws Exception {
+		Funcion funcion = dao.traerFuncion(idFuncion);
+		if(funcion == null)throw new Exception("La funcion no existe");
+		return funcion;
+	}
+	
+	public List<Funcion> traerCodigoDescuento(){
+		return dao.traerFuncion();
+	}
+	
+	public int agregar(double precioBase, GregorianCalendar diaHora,Evento evento,double descuento, List<CodigoDescuento> lstCodDesc,DiaDescuento diaDescuento) {
+		Funcion funcion = new Funcion(precioBase,diaHora,evento,descuento,diaDescuento,lstCodDesc);
+		return dao.agregar(funcion);
+	}
+	
+	public void modificar(Funcion funcion) {
+		dao.actualizar(funcion);
+	}
+	
+	public void eliminar(int idFuncion)throws Exception {
+		Funcion funcion = dao.traerFuncion(idFuncion);
+		if(funcion == null)throw new Exception("La funcion no existe");
+		dao.eliminar(funcion);
+	}
 }
