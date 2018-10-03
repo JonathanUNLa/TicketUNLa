ab60923c4180f7e56efd32b3126b9a858228b238
LuceroJose <33098113+LuceroJose@users.noreply.github.com>
2018-10-03 20:42:22 -0300

    test nuevo

--- TicketUNLa/src/test/TestMain.java
+++ TicketUNLa/src/test/TestMain.java
@@ -35,43 +35,51 @@ public class TestMain {
 		try {
 			
 			// agregar 
-		/*	tipoAuditorioAbm.agregar("estadio");
+			
+			/*reservaAbm.agregar(usuarioAbm.traerUsuarioHql(1), false);
+			tipoAuditorioAbm.agregar("Vacio");
+			auditorioAbm.agregar("Auditorio Vacio", tipoAuditorioAbm.traerTipoAuditorio(1));
+			seccionAbm.agregar("seccion Vacia", 0, auditorioAbm.traerAuditorio(1));
+			diaDescuentoAbm.agregar("Ninguno", 0);
+			tipoBeneficioAbm.agregar("Ninguno", 0);
+			
+			tipoAuditorioAbm.agregar("estadio");
 			tipoAuditorioAbm.agregar("teatro");
 			tipoAuditorioAbm.agregar("cine");
-			auditorioAbm.agregar("El Monumental", tipoAuditorioAbm.traerTipoAuditorio(1));
-			auditorioAbm.agregar("Colon", tipoAuditorioAbm.traerTipoAuditorio(2));
-			seccionAbm.agregar("seccion 1", 0, auditorioAbm.traerAuditorio(1));
-			seccionAbm.agregar("seccion 2", 100, auditorioAbm.traerAuditorio(1));
-			seccionAbm.agregar("seccion 3", 200, auditorioAbm.traerAuditorio(1));
-			butacaAbm.agregar(2, 1, seccionAbm.traerSeccion(1));
-			butacaAbm.agregar(2, 2, seccionAbm.traerSeccion(1));
-			butacaAbm.agregar(1, 1, seccionAbm.traerSeccion(1));
-			butacaAbm.agregar(1, 2, seccionAbm.traerSeccion(1));
+			auditorioAbm.agregar("El Monumental", tipoAuditorioAbm.traerTipoAuditorio(2));
+			auditorioAbm.agregar("Colon", tipoAuditorioAbm.traerTipoAuditorio(3));
+			seccionAbm.agregar("seccion 1", 0, auditorioAbm.traerAuditorio(2));
+			seccionAbm.agregar("seccion 2", 100, auditorioAbm.traerAuditorio(2));
+			seccionAbm.agregar("seccion 3", 200, auditorioAbm.traerAuditorio(2));
 			butacaAbm.agregar(2, 1, seccionAbm.traerSeccion(2));
 			butacaAbm.agregar(2, 2, seccionAbm.traerSeccion(2));
 			butacaAbm.agregar(1, 1, seccionAbm.traerSeccion(2));
 			butacaAbm.agregar(1, 2, seccionAbm.traerSeccion(2));
 			butacaAbm.agregar(2, 1, seccionAbm.traerSeccion(3));
-			eventoAbm.agregar("Evento 1", auditorioAbm.traerAuditorio(1));
-			eventoAbm.agregar("Evento 2", auditorioAbm.traerAuditorio(2));
+			butacaAbm.agregar(2, 2, seccionAbm.traerSeccion(3));
+			butacaAbm.agregar(1, 1, seccionAbm.traerSeccion(3));
+			butacaAbm.agregar(1, 2, seccionAbm.traerSeccion(3));
+			butacaAbm.agregar(2, 1, seccionAbm.traerSeccion(3));
+			eventoAbm.agregar("Evento 1", auditorioAbm.traerAuditorio(2));
+			eventoAbm.agregar("Evento 2", auditorioAbm.traerAuditorio(3));
 			diaDescuentoAbm.agregar("Lunes", 15.0);
 			diaDescuentoAbm.agregar("Martes", 20.0);
 			funcionAbm.agregar(500.0, new GregorianCalendar(), eventoAbm.traerEvento(1), diaDescuentoAbm.traerDiaDescuento(1));
 			funcionAbm.agregar(550.0, new GregorianCalendar(), eventoAbm.traerEvento(1), diaDescuentoAbm.traerDiaDescuento(1));
 			funcionAbm.agregar(600.0, new GregorianCalendar(), eventoAbm.traerEvento(2), diaDescuentoAbm.traerDiaDescuento(2));
 			funcionAbm.agregar(650.0, new GregorianCalendar(), eventoAbm.traerEvento(2), diaDescuentoAbm.traerDiaDescuento(2));
-			codigoDescuentoAbm.agregar("SDE5697", 100.0, seccionAbm.traerSeccion(1), funcionAbm.traerFuncion(1));
+			codigoDescuentoAbm.agregar("SDE5697", 100.0, seccionAbm.traerSeccion(2), funcionAbm.traerFuncion(1));
 			tipoUsuarioAbm.agregar("Administrador");
 			tipoUsuarioAbm.agregar("Cliente");
 			tipoUsuarioAbm.agregar("Empleado");
 			tipoBeneficioAbm.agregar("Jubilado", 10);
 			tipoBeneficioAbm.agregar("Estudiante", 5);
-			usuarioAbm.agregar(30, "Jonathan", "Yañez", "43698789", "Jon", "soyputo123", tipoUsuarioAbm.traerTipoUsuario(2), tipoBeneficioAbm.traerTipoBeneficio(1),null);
-			usuarioAbm.agregar(40, "Jose", "Lucero", "40364587", "asd", "123456", tipoUsuarioAbm.traerTipoUsuario(1), null,null);
-			usuarioAbm.agregar(50, "Empleado", "Asd", "43698789", "asdasd", "asd123", tipoUsuarioAbm.traerTipoUsuario(3), null,null);
+			usuarioAbm.agregar(30, "Jonathan", "Yañez", "43698789", "Jon", "soyputo123", tipoUsuarioAbm.traerTipoUsuario(2), tipoBeneficioAbm.traerTipoBeneficio(2),auditorioAbm.traerAuditorio(1));
+			usuarioAbm.agregar(40, "Jose", "Lucero", "40364587", "asd", "123456", tipoUsuarioAbm.traerTipoUsuario(1), tipoBeneficioAbm.traerTipoBeneficio(1),auditorioAbm.traerAuditorio(1));
+			usuarioAbm.agregar(50, "Empleado", "Asd", "43698789", "asdasd", "asd123", tipoUsuarioAbm.traerTipoUsuario(3), tipoBeneficioAbm.traerTipoBeneficio(1),auditorioAbm.traerAuditorio(1));
 			
-			reservaAbm.agregar(usuarioAbm.traerUsuarioHql(1), false);
-			*/
+			reservaAbm.agregar(usuarioAbm.traerUsuarioHql(1), false);*/
+			
 			//traer e imprimir
 			/*
 			System.out.println(butacaAbm.traerButacaHql(1));
@@ -83,7 +91,7 @@ public class TestMain {
 			System.out.println(usuarioAbm.traerUsuarioHql(1));
 			
 			System.out.println(entradaAbm.traerEntradaHql(1));
-			m
+			
 			*/
 			
 			//entradaAbm.agregar(butacaAbm.traerButacaHql(1),  funcionAbm.traerFuncionHql(1),"SDE5697",reservaAbm.traerReserva(1));
